e038d4bfaa8bcf965165d35fa7ba00e3bee300af
Turgho <victor.hugo3692111@gmail.com>
2026-02-22 03:29:40 -0300

    Add TicketMessages model for managing ticket-related messages

---
+++ internal/models/ticket_messages.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type TicketMessages struct {
+	ID        string    `gorm:"primaryKey;type:char(36)" json:"id"`
+	TicketID  string    `gorm:"type:char(36);not null" json:"ticket_id"`
+	SenderID  string    `gorm:"type:char(36);not null" json:"sender_id"`
+	Message   string    `gorm:"type:text;not null" json:"message"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+}
